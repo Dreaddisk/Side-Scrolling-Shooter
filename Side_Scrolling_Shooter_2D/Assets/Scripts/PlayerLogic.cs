367826452fefcf365ebd5d37c7a0e47a53b87ec0
Dreaddisk <joey_manuel@outlook.com>
2018-10-27 11:28:40 -0400

    2. Player Movement
    Cerated a simple Player Movement.

---
+++ Side_Scrolling_Shooter_2D/Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    #region Variables
+    public float playerSpeed = 3.0f;
+
+    float maxPlayerY = 7.0f;
+    float maxPlayerX = 25.0f;
+    #endregion
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if(Input.GetKey(KeyCode.W) && transform.position.y <= maxPlayerY)
+        {
+            transform.Translate(0, playerSpeed, 0);
+        }
+        if(Input.GetKey(KeyCode.A) && transform.position.x >= -maxPlayerX)
+        {
+            transform.Translate(-playerSpeed, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.S) && transform.position.y >= -maxPlayerY)
+        {
+            transform.Translate(0, -playerSpeed, 0);
+        }
+        if(Input.GetKey(KeyCode.D) && transform.position.x <= maxPlayerX)
+        {
+            transform.Translate(playerSpeed, 0, 0);
+        }
+    }
+}// main class
